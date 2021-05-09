aaca7206749636df1f7ccda7d9add059db04adc4
Gautham <gauthamramesh3110@gmail.com>
2021-05-09 16:35:01 +0530

    Refactor

--- licenseplate_recognition/licenseplate_recognition.py
+++ licenseplate_recognition/licenseplate_recognition.py
@@ -13,14 +13,14 @@ from tqdm import tqdm
 def load_models():
 
     options = {
-        "pbLoad": "./weights/licenseplate_localizer/yolo-licenseplate.pb",
-        "metaLoad": "./weights/licenseplate_localizer/yolo-licenseplate.meta",
+        "pbLoad": "./licenseplate_recognition/weights/licenseplate_localizer/yolo-licenseplate.pb",
+        "metaLoad": "./licenseplate_recognition/weights/licenseplate_localizer/yolo-licenseplate.meta",
         "threshold": 0.1,
         "gpu": 0.0,
     }
 
     licenseplate_localizer = TFNet(options)
-    character_classifier = load_model("./weights/character_classifier/character_classifier.h5")
+    character_classifier = load_model("./licenseplate_recognition/weights/character_classifier/character_classifier.h5")
 
     return licenseplate_localizer, character_classifier
 
